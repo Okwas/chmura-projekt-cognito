b3dcb3a74e6d9e13bcf9da3b1ec93c0c748700e8
demon <naimadokwas@gmail.com>
2024-05-14 09:52:18 +0200

    login text change

--- TicTacToe/tictactoe-frontend/src/pages/login/Login.tsx
+++ TicTacToe/tictactoe-frontend/src/pages/login/Login.tsx
@@ -1,69 +1,93 @@
-import { useRef, useState } from "react";
-import { useAppStore } from "../../stores/app-store";
-import { useGameStore } from "../../stores/game-store";
-import { useUserStore } from "@/stores/user-store";
-import { authenticate } from "@/api/cognito";
+import { useRef, useState } from "react"
+import { useAppStore } from "../../stores/app-store"
+import { useGameStore } from "../../stores/game-store"
+import { useUserStore } from "@/stores/user-store"
+import { authenticate } from "@/api/cognito"
 
 export default function Login() {
-    const { joinGame, joinGameLoading } = useAppStore();
-    const { user, setUser } = useUserStore();
-    const { setStage } = useAppStore();
+  const { joinGame, joinGameLoading } = useAppStore()
+  const { user, setUser } = useUserStore()
+  const { setStage } = useAppStore()
 
-    const emailRef = useRef<HTMLInputElement>(null);
-    const passwordRef = useRef<HTMLInputElement>(null);
-    
-    const [error, setError] = useState<string>('');
+  const emailRef = useRef<HTMLInputElement>(null)
+  const passwordRef = useRef<HTMLInputElement>(null)
 
-    function onLoginClick() {
-      const email = emailRef.current?.value;
-      const password = passwordRef.current?.value;
-      if (!email || !password) {
-        setError('Please fill in all fields');
-        return;
-      }
-      login(email,password,(user) => {
-        setUser(user);
-      }, (error) => {
-        setError(error);
-      });
+  const [error, setError] = useState<string>("")
+
+  function onLoginClick() {
+    const email = emailRef.current?.value
+    const password = passwordRef.current?.value
+    if (!email || !password) {
+      setError("Please fill in all fields")
+      return
     }
-  
-    function login(email: string, password: string, onSuccess?: (user: User) => void, onError?: (error: string) => void){
-      authenticate(email, password)
+    login(
+      email,
+      password,
+      (user) => {
+        setUser(user)
+      },
+      (error) => {
+        setError(error)
+      }
+    )
+  }
+
+  function login(
+    email: string,
+    password: string,
+    onSuccess?: (user: User) => void,
+    onError?: (error: string) => void
+  ) {
+    authenticate(email, password)
       .then((session) => {
-        onSuccess && onSuccess({
-          email: email,
-          accessToken: session.getAccessToken().getJwtToken(),
-          refreshToken: session.getRefreshToken().getToken()
-        });
-        console.log('success');
-        console.log(session);
+        onSuccess &&
+          onSuccess({
+            email: email,
+            accessToken: session.getAccessToken().getJwtToken(),
+            refreshToken: session.getRefreshToken().getToken(),
+          })
+        console.log("success")
+        console.log(session)
       })
       .catch((error) => {
-        console.log('error');
-        console.log(error);
-        onError && onError(error.message);
-      });
-    }
+        console.log("error")
+        console.log(error)
+        onError && onError(error.message)
+      })
+  }
 
-    return (
-        <div>
-            <div>
-                <p>Login  </p>
-                <input type="email" placeholder='email' defaultValue={"user2@example.com"} ref={emailRef} />
-                <input type="password" placeholder='password' defaultValue={"123456"} ref={passwordRef} />
-                <button onClick={() => onLoginClick()}>Login</button>
-                <p>{error}</p>
-                <button onClick={() => setStage('register')}>Go to register</button>
-            </div>
-            <div className="flex flex-col gap-4">
-                <button disabled={!user} onClick={() => {
-                    joinGame({ username: user!.email }, user!);
-                }}>
-                    Play
-                </button>
-                {joinGameLoading && <div>Loading...</div>}
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div>
+        <p>Login </p>
+        <input
+          type="email"
+          placeholder="email"
+          defaultValue={"user2@example.com"}
+          ref={emailRef}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          defaultValue={"123456"}
+          ref={passwordRef}
+        />
+        <button onClick={() => onLoginClick()}>Login</button>
+        <p>{error}</p>
+        <button onClick={() => setStage("register")}>Go to register</button>
+      </div>
+      <div className="flex flex-col gap-4">
+        <button
+          disabled={!user}
+          onClick={() => {
+            joinGame({ username: user!.email }, user!)
+          }}
+        >
+          Play now
+        </button>
+        {joinGameLoading && <div>Loading... Please wait</div>}
+      </div>
+    </div>
+  )
 }
